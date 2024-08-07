825f39226470b85f11917a93620b6342c5168dba
n1rasa00 <n1rasa00@students.oamk.fi>
2024-08-07 07:09:07 -0400

    module 3 part1

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,5 +1,4 @@
-# Uncomment the required imports before adding the code
-
+from .models import CarMake, CarModel
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
